5d7a35759fc3a9ee2eff94a89aa844029756c3a4
olsohee <dlthgml0108@daum.net>
2023-11-11 01:47:46 +0900

    test: 총주문 금액 계산 기능에 대한 테스트 코드 작성

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import christmas.domain.menu.Menu;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,5 +22,21 @@ class OrderTest {
 
         // then
         Assertions.assertThat(order).isNotNull();
+    }
+
+    @DisplayName("총주문 금액 계산 기능")
+    @Test
+    void calculateTotalOrderPrice() {
+        // given
+        OrderMenu tapas = new OrderMenu("타파스", 1);
+        OrderMenu chocoCake = new OrderMenu("초코케이크", 2);
+        List<OrderMenu> orderMenus = List.of(tapas, chocoCake);
+
+        // when
+        Order order = new Order(orderMenus);
+
+        // then
+        Assertions.assertThat(order.getTotalOrderPrice()).isEqualTo(
+                Menu.TAPAS.getPrice() + Menu.CHOCO_CAKE.getPrice() * 2);
     }
 }
