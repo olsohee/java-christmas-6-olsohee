6d7e5b25169781e08c397240e5a3a6d1d0e83142
olsohee <dlthgml0108@daum.net>
2023-11-11 01:57:36 +0900

    test(DateTest): 날짜 생성 기능에 대한 테스트 코드 작성

---
+++ src/test/java/christmas/domain/DateTest.java
@@ -0,0 +1,27 @@
+package christmas.domain;
+
+import christmas.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+    @DisplayName("날짜 생성")
+    @Test
+    void createDate() {
+        // given
+        Date date = new Date(1);
+
+        // then
+        Assertions.assertThat(date).isNotNull();
+    }
+
+    @DisplayName("1 ~ 31 이외의 숫자이면 날짜 생성 실패")
+    @Test
+    void createDateFail() {
+        Assertions.assertThatThrownBy(() -> new Date(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_DATE.getErrorMessage());
+    }
+}
