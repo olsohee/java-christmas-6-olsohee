8b36dd5126a7a9695374cec59770d92275010f96
olsohee <dlthgml0108@daum.net>
2023-11-11 12:33:32 +0900

    test(EventTest): 적용되는 이벤트로 총혜택 금액을 계산하는 기능에 대한 테스트 코드 작성

--- src/test/java/christmas/domain/EventTest.java
+++ src/test/java/christmas/domain/EventTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import christmas.domain.menu.Menu;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,5 +28,31 @@ class EventTest {
         // then
         Assertions.assertThat(events.size()).isEqualTo(4);
         Assertions.assertThat(events.containsAll(List.of(Event.CHRISTMAS, Event.WEEKDAY, Event.SPECIAL, Event.GIFT)));
+    }
+
+    @DisplayName("주문 내역과 이벤트 리스트를 통해 총혜택 금액 반환")
+    @Test
+    void getTotalBenefitAmount() {
+        // given
+        Order order = new Order(
+                List.of(new OrderMenu("타파스", 1),
+                        new OrderMenu("티본스테이크", 1),
+                        new OrderMenu("해산물파스타", 2),
+                        new OrderMenu("초코케이크", 1))
+        );
+
+        Date date = new Date(3);
+
+        List<Event> events = List.of(Event.CHRISTMAS, Event.WEEKDAY, Event.SPECIAL, Event.GIFT);
+
+        // when
+        int totalBenefitAmount = events.stream()
+                .mapToInt(event -> event.getBenefitAmount(order, date))
+                .sum();
+
+        // then
+        Assertions.assertThat(totalBenefitAmount)
+                .isEqualTo(date.getChristmasEventBenefitAmount() + order.getWeekdayEventBenefitAmount()
+                        + 1000 + Menu.CHAMPAGNE.getPrice());
     }
 }
