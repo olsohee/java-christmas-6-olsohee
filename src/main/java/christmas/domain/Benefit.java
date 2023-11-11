e9e2010beeb945bc01879168388a2a420040ef1d
olsohee <dlthgml0108@daum.net>
2023-11-11 12:47:37 +0900

    feat(Benefit): Event의 메소드를 호출하고, 총할인 금액을 계산하는 기능 구현

---
+++ src/main/java/christmas/domain/Benefit.java
@@ -0,0 +1,33 @@
+package christmas.domain;
+
+import christmas.domain.menu.Menu;
+
+import java.util.List;
+
+public class Benefit {
+
+    List<Event> events;
+    int totalBenefitAmount;
+
+    public Benefit(Order order, Date date) {
+        events = getApplicableEvents(order, date);
+        totalBenefitAmount = getTotalBenefitAmount(order, date);
+    }
+
+    private List<Event> getApplicableEvents(Order order, Date date) {
+        return Event.getApplicableEvents(order, date);
+    }
+
+    private int getTotalBenefitAmount(Order order, Date date) {
+        return events.stream()
+                .mapToInt(event -> event.getBenefitAmount(order, date))
+                .sum();
+    }
+
+    public int getTotalDiscountAmount() {
+        if(events.contains(Event.GIFT)) {
+            return totalBenefitAmount - Menu.CHAMPAGNE.getPrice();
+        }
+        return totalBenefitAmount;
+    }
+}
