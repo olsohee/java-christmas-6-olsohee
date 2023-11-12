fc3dc0426d5f30a8a15f0661928d20671704b297
olsohee <dlthgml0108@daum.net>
2023-11-12 11:48:18 +0900

    feat: 주문 결과 출력 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,28 @@
+package christmas.view;
+
+import christmas.dto.BadgeDto;
+import christmas.dto.DateDto;
+import christmas.dto.PaymentDto;
+import christmas.dto.benefitDto.BenefitsDto;
+import christmas.dto.orderMenuDto.OrderDto;
+
+public interface OutputView {
+
+    void printStartMessage(DateDto dateDto);
+
+    void printOrder(OrderDto orderDto);
+
+    void printBenefits(BenefitsDto benefitsDto);
+
+    void printPayment(PaymentDto paymentDto);
+
+    void printBadge(BadgeDto badgeDto);
+
+    default void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    default void printNewLine() {
+        System.out.println();
+    }
+}
