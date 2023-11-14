93fdee303fa1e36a9076e92512cf0cecf7d204f1
olsohee <dlthgml0108@daum.net>
2023-11-14 16:07:03 +0900

    test(EventTest): 테스트 코드 리팩토링

--- src/test/java/christmas/domain/EventTest.java
+++ src/test/java/christmas/domain/EventTest.java
@@ -1,7 +1,12 @@
 package christmas.domain;
 
+import christmas.domain.event.Event;
 import christmas.domain.menu.Menu;
+import christmas.domain.order.Date;
+import christmas.domain.order.OrderMenus;
+import christmas.domain.order.TotalOrderPrice;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,51 +16,51 @@ import java.util.Map;
 
 class EventTest {
 
-    @DisplayName("주문 내역과 방문 날짜를 통해 적용되는 이벤트 리스트 반환")
-    @Test
-    void getApplicableEvents() {
-        // given
+    private Date date;
+    private OrderMenus orderMenus;
+    private TotalOrderPrice totalOrderPrice;
+
+    @BeforeEach
+    void init() {
+        date = new Date(3);
+
         Map<String, Integer> orderMenuAndCount = new HashMap<>();
         orderMenuAndCount.put("타파스", 1);
         orderMenuAndCount.put("티본스테이크", 1);
         orderMenuAndCount.put("해산물파스타", 2);
         orderMenuAndCount.put("초코케이크", 1);
-        Order order = new Order(orderMenuAndCount);
+        orderMenus = new OrderMenus(orderMenuAndCount);
 
-        Date date = new Date(3);
+        totalOrderPrice = new TotalOrderPrice(orderMenus.calculateTotalOrderPrice());
+    }
 
-        // when
-        List<Event> events = Event.getApplicableEvents(order, date);
+    @DisplayName("적용되는 이벤트 리스트 생성")
+    @Test
+    void getApplicableEvents() {
+        // given
+        List<Event> events = Event.getApplicableEvents(date, orderMenus, totalOrderPrice);
 
         // then
         Assertions.assertThat(events.size()).isEqualTo(4);
         Assertions.assertThat(events.containsAll(List.of(Event.CHRISTMAS, Event.WEEKDAY, Event.SPECIAL, Event.GIFT)));
     }
 
-    @DisplayName("주문 내역과 이벤트 리스트를 통해 총혜택 금액 반환")
+    @DisplayName("적용되는 이벤트 리스트를 통해 총혜택 금액 반환")
     @Test
     void getTotalBenefitAmount() {
         // given
-        Map<String, Integer> orderMenuAndCount = new HashMap<>();
-        orderMenuAndCount.put("타파스", 1);
-        orderMenuAndCount.put("티본스테이크", 1);
-        orderMenuAndCount.put("해산물파스타", 2);
-        orderMenuAndCount.put("초코케이크", 1);
-
-        Order order = new Order(orderMenuAndCount);
-
-        Date date = new Date(3);
-
-        List<Event> events = List.of(Event.CHRISTMAS, Event.WEEKDAY, Event.SPECIAL, Event.GIFT);
+        List<Event> events = Event.getApplicableEvents(date, orderMenus, totalOrderPrice);
 
         // when
         int totalBenefitAmount = events.stream()
-                .mapToInt(event -> event.getBenefitAmount(order, date))
+                .mapToInt(event -> event.getBenefitAmount(date, orderMenus))
                 .sum();
 
         // then
         Assertions.assertThat(totalBenefitAmount)
-                .isEqualTo(date.getChristmasEventBenefitAmount() + order.getWeekdayEventBenefitAmount()
-                        + 1000 + Menu.CHAMPAGNE.getPrice());
+                .isEqualTo(date.getChristmasEventBenefitAmount()
+                        + orderMenus.getWeekdayEventBenefitAmount()
+                        + 1000
+                        + Menu.CHAMPAGNE.getPrice());
     }
 }
