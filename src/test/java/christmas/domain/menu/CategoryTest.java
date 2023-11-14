215cb5576f0cb622a8c4f4db99913b6b6e683ab2
olsohee <dlthgml0108@daum.net>
2023-11-14 15:55:06 +0900

    test(CategoryTest): Category 클래스에 대한 테스트 코드 작성

---
+++ src/test/java/christmas/domain/menu/CategoryTest.java
@@ -0,0 +1,33 @@
+package christmas.domain.menu;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class CategoryTest {
+
+    @DisplayName("음료 메뉴인지 판단")
+    @EnumSource(value = Menu.class, names = {"ZERO_COKE", "RED_WINE", "CHAMPAGNE"})
+    @ParameterizedTest
+    void isDrinkMenu(Menu menu) {
+        Assertions.assertThat(Category.isDrinkMenu(menu))
+                .isTrue();
+    }
+
+    @DisplayName("디저트 메뉴인지 판단")
+    @EnumSource(value = Menu.class, names = {"CHOCO_CAKE", "ICE_CREAM"})
+    @ParameterizedTest
+    void isDessertMenu(Menu menu) {
+        Assertions.assertThat(Category.isDessertMenu(menu))
+                .isTrue();
+    }
+
+    @DisplayName("메인 메뉴인지 판단")
+    @EnumSource(value = Menu.class, names = {"T_BORN_STAKE", "BARBECUE_RIP", "SEAFOOD_PASTA", "CHRISTMAS_PASTA"})
+    @ParameterizedTest
+    void isMainMenu(Menu menu) {
+        Assertions.assertThat(Category.isMainMenu(menu))
+                .isTrue();
+    }
+}
