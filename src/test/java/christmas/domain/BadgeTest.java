75bd4146602706da16b043c8b27e319bc71fcb64
olsohee <dlthgml0108@daum.net>
2023-11-14 16:14:05 +0900

    test(BadgeTest): 테스트 코드 리팩토링

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -1,24 +1,39 @@
 package christmas.domain;
 
+import christmas.domain.benefit.Badge;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BadgeTest {
 
     @DisplayName("총혜택 금액에 따른 배지 계산")
-    @Test
-    void getBadge() {
-        // given
-        Badge startBadge = Badge.getBadge(5000);
-        Badge treeBadge = Badge.getBadge(10000);
-        Badge santaBadge = Badge.getBadge(20000);
-        Badge notApplicable = Badge.getBadge(4000);
+    @ValueSource(ints = {0, 4999})
+    @ParameterizedTest
+    void getBadgeNotApplicable(int totalBenefitAmount) {
+        Assertions.assertThat(Badge.getBadge(totalBenefitAmount))
+                .isEqualTo(Badge.NOT_APPLICABLE);
+    }
 
-        // then
-        Assertions.assertThat(startBadge).isEqualTo(Badge.STAR);
-        Assertions.assertThat(treeBadge).isEqualTo(Badge.TREE);
-        Assertions.assertThat(santaBadge).isEqualTo(Badge.SANTA);
-        Assertions.assertThat(notApplicable).isEqualTo(Badge.NOT_APPLICABLE);
+    @ValueSource(ints = {5000, 5001, 9999})
+    @ParameterizedTest
+    void getBadgeStar(int totalBenefitAmount) {
+        Assertions.assertThat(Badge.getBadge(totalBenefitAmount))
+                .isEqualTo(Badge.STAR);
+    }
+
+    @ValueSource(ints = {10000, 10001, 19999})
+    @ParameterizedTest
+    void getBadgeTree(int totalBenefitAmount) {
+        Assertions.assertThat(Badge.getBadge(totalBenefitAmount))
+                .isEqualTo(Badge.TREE);
+    }
+
+    @ValueSource(ints = {20000, 20001})
+    @ParameterizedTest
+    void getBadgeSanta(int totalBenefitAmount) {
+        Assertions.assertThat(Badge.getBadge(totalBenefitAmount))
+                .isEqualTo(Badge.SANTA);
     }
 }
--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,42 +1,44 @@
-package christmas.domain;
-
-import christmas.domain.menu.Menu;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-class OrderTest {
-
-    @DisplayName("주문 생성")
-    @Test
-    void createOrder() {
-        // given
-        OrderMenu tapas = new OrderMenu("타파스", 1);
-        OrderMenu chocoCake = new OrderMenu("초코케이크", 2);
-        List<OrderMenu> orderMenus = List.of(tapas, chocoCake);
-
-        // when
-        Order order = new Order(orderMenus);
-
-        // then
-        Assertions.assertThat(order).isNotNull();
-    }
-
-    @DisplayName("총주문 금액 계산 기능")
-    @Test
-    void calculateTotalOrderPrice() {
-        // given
-        OrderMenu tapas = new OrderMenu("타파스", 1);
-        OrderMenu chocoCake = new OrderMenu("초코케이크", 2);
-        List<OrderMenu> orderMenus = List.of(tapas, chocoCake);
-
-        // when
-        Order order = new Order(orderMenus);
-
-        // then
-        Assertions.assertThat(order.getTotalOrderPrice()).isEqualTo(
-                Menu.TAPAS.getPrice() + Menu.CHOCO_CAKE.getPrice() * 2);
-    }
-}
+//package christmas.domain;
+//
+//import christmas.domain.menu.Menu;
+//import christmas.domain.order.Order;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//class OrderTest {
+//
+//    @DisplayName("주문 생성")
+//    @Test
+//    void createOrder() {
+//        // given
+//        Map<String, Integer> orderMenuAndCount = new HashMap<>();
+//        orderMenuAndCount.put("타파스", 1);
+//        orderMenuAndCount.put("초코케이크", 2);
+//
+//        // when
+//        Order order = new Order(orderMenuAndCount);
+//
+//        // then
+//        Assertions.assertThat(order).isNotNull();
+//    }
+//
+//    @DisplayName("총주문 금액 계산 기능")
+//    @Test
+//    void calculateTotalOrderPrice() {
+//        // given
+//        Map<String, Integer> orderMenuAndCount = new HashMap<>();
+//        orderMenuAndCount.put("타파스", 1);
+//        orderMenuAndCount.put("초코케이크", 2);
+//
+//        // when
+//        Order order = new Order(orderMenuAndCount);
+//
+//        // then
+//        Assertions.assertThat(order.getTotalOrderPrice()).isEqualTo(
+//                Menu.TAPAS.getPrice() + Menu.CHOCO_CAKE.getPrice() * 2);
+//    }
+//}
